018b7b572f348b482b022f76841715d8ed3bdcfe
Shpuntoff <v.shpuntoff@gmail.com>
2023-02-24 08:07:41 +0200

    Cloudinary - done

--- src/services/auth/changeAvatar.js
+++ src/services/auth/changeAvatar.js
@@ -1,16 +1,16 @@
 const { User } = require("../../db/userModel");
 const { ValidationError } = require("../../helpers/errors");
+const { cloudUpload } = require("../cloudUpload")
 
-const path = require("path");
 const fs = require("fs").promises;
-const Jimp = require("jimp");
 
 const changeAvatar = async (file, id) => {
+  
+
   if (!file) {
     throw new ValidationError("transfer file, please");
   }
 
-  const storeImage = path.resolve("./public/avatars");
   const { path: temporaryName } = file;
   const [, extension] = temporaryName?.split(".");
 
@@ -19,41 +19,20 @@ const changeAvatar = async (file, id) => {
     throw new ValidationError("file must be '.jpg' or '.png'");
   }
 
-  const newName = id + "." + extension;
-  const fileName = path.join(storeImage, newName);
-
   try {
-    const avatarDir = await fs.readdir(storeImage);
-    const oldAvatar = avatarDir.find((el) => el.includes(id));
+    const {path, fieldname} = file;
+    const {url: avatarUrl} = await cloudUpload(path, fieldname, id);
 
-    await fs.rename(temporaryName, fileName);
-
-    if (oldAvatar) {
-      const [, extension] = fileName?.split(".");
-      const [, oldExtension] = oldAvatar.split(".");
-      if (extension !== oldExtension)
-        await fs.unlink(storeImage + "/" + oldAvatar);
-    }
-
-    Jimp.read(fileName, (err, avatar) => {
-      if (err) throw err;
-      avatar
-        .resize(250, 250) // resize
-        .write(fileName); // save
-    });
-
-    const avatarPath = path.join("avatars", newName);
-
-    const { avatarURL } = await User.findOneAndUpdate(
+    await User.findOneAndUpdate(
       id,
       {
-        avatarURL: avatarPath.replace(/\\/g, "/"),
+        avatarURL: avatarUrl,
       },
       {
         new: true,
       }
     );
-    return avatarURL;
+    return avatarUrl;
   } catch (error) {
     throw new ValidationError("Load file error");
   }
---
+++ src/services/cloudUpload.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
+});
+
+const cloudUpload = async (file, folder, filename) => {
+    const cloudFilePath = folder + "/" + filename;
+
+    const options = {
+        public_id: cloudFilePath,
+        overwrite: true,
+        transformation: [
+            { width: 250, height: 250 }
+        ]
+    };
+
+    try {
+        const {url} = await cloudinary.uploader.upload(file, options);
+        await fs.unlinkSync(file);
+
+        return {
+            message: 'Upload success',
+            url
+        }
+    } catch (error) {
+        fs.unlinkSync(file);
+        return {
+            message: 'Upload fail',
+            error
+        };
+    }
+};
+
+module.exports = {cloudUpload};
