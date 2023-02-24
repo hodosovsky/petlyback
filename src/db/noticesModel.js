8cb37526f1a7f08a9f5b075b6f74234769986a61
mku777 <mkm.kuz777@gmail.com>
2023-02-24 11:46:38 +0200

    model +  add handlemongoose err in helpers from hw

---
+++ src/db/noticesModel.js
@@ -0,0 +1,98 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const handleSaveErrors = require("../helpers/handleSaveErrors");
+
+const regexDate =
+  /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
+  const locationRegExp =
+  /^[A-ZА-ЯЁАЩЬЮЯҐЄІЇ][a-zA-Zа-яёА-ЯЁА-ЩЬЮЯҐЄІЇа-щьюяґєії\-]+,\s?[A-ZА-ЯЁАЩЬЮЯҐЄІЇ][a-zA-Zа-яёА-ЯЁА-ЩЬЮЯҐЄІЇа-щьюяґєії]+$/;
+
+const noticesShema = new Schema(
+  {
+    categoryName: {
+      type: String,
+      enum: ["sell", "lost/found", "in good hands"],
+      required: [true, "Notice type is required"],
+    },
+
+    title: {
+      type: String,
+      minLength: 2,
+      maxLength: 48,
+      required: [true, "Title is required"],
+    },
+
+    name: {
+      type: String,
+      minLength: 2,
+      maxLength: 16,
+      default: null,
+    },
+
+    birthdate: {
+      type: String,
+      default: null,
+    },
+
+    breed: {
+      type: String,
+      minLength: 2,
+      maxLength: 24,
+      default: null,
+    },
+
+    sex: {
+      type: String,
+      enum: ["male", "female"],
+      require: true,
+    },
+
+    location: {
+      type: String,
+      default: null,
+    },
+
+    price: {
+      type: Number,
+      default: null,
+    },
+
+    avatar: {
+      type: String,
+      default: null,
+    },
+
+    comments: {
+      type: String,
+      minLength: 8,
+      maxLength: 120,
+      required: [true, "Comments is required"],
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+noticesShema.post("save", handleSaveErrors);
+
+const noticeValidateSchema = Joi.object({
+  birthdate: Joi.string().pattern(new RegExp(regexDate)),
+  location: Joi.string().pattern(locationRegExp),
+  price: Joi.number().min(1),
+});
+
+const schemas = {
+  noticeValidateSchema,
+};
+
+const Notices = model("notices", noticesShema);
+
+module.exports = {
+  Notices,
+  schemas,
+};
