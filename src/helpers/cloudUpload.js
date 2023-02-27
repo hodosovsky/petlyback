b9cf35db7d993fe743e711951b7d7c85b3b1df58
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-02-27 19:37:52 +0200

    remove transformation file

--- src/helpers/cloudUpload.js
+++ src/helpers/cloudUpload.js
@@ -1,39 +1,39 @@
-const fs = require('fs')
-const cloudinary = require('cloudinary').v2
-const dotenv = require('dotenv')
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_KEY,
   api_secret: process.env.CLOUD_SECRET,
-})
+});
 
 const cloudUpload = async (file, folder, filename) => {
-  const cloudFilePath = folder + '/' + filename
+  const cloudFilePath = folder + "/" + filename;
 
   const options = {
     public_id: cloudFilePath,
     overwrite: true,
-    transformation: [{ width: 600, height: 600, crop: "fit" }],
-  }
+    // transformation: [{ width: 600, height: 600, crop: "fit" }],
+  };
 
   try {
-    const { url } = await cloudinary.uploader.upload(file, options)
-    await fs.unlinkSync(file)
+    const { url } = await cloudinary.uploader.upload(file, options);
+    await fs.unlinkSync(file);
 
     return {
-      message: 'Upload success',
+      message: "Upload success",
       url,
-    }
+    };
   } catch (error) {
-    fs.unlinkSync(file)
+    fs.unlinkSync(file);
     return {
-      message: 'Upload fail',
+      message: "Upload fail",
       error,
-    }
+    };
   }
-}
+};
 
-module.exports = { cloudUpload }
+module.exports = { cloudUpload };
