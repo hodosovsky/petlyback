1bcdf16a50f8f8dbfee7d863ba1d3f0abaf2509c
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-02-27 11:18:36 +0200

    add sort news by date

--- src/services/news/getAllNewsService.js
+++ src/services/news/getAllNewsService.js
@@ -1,13 +1,16 @@
-const { News } = require('../../db/newsModel')
-
+const { News } = require("../../db/newsModel");
+const { WrongParametersError } = require("../../helpers/errors");
 const getAllNewsService = async (search) => {
   if (search) {
-    const data = await News.find({ title: { $regex: search, $options: 'i' } })
-    return data
+    const data = await News.find({
+      title: { $regex: search, $options: "i" },
+    }).sort({ date: -1 });
+    if (!data.length) throw new WrongParametersError("News not found");
+    return data;
   }
 
-  const data = await News.find({})
-  return data
-}
+  const data = await News.find({});
+  return data;
+};
 
-module.exports = { getAllNewsService }
+module.exports = { getAllNewsService };
