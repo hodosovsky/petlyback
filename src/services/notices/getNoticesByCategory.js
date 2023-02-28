b1527b4049c96de025564e048455993347fb2228
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-02-28 11:55:30 +0200

    add get notices by id

--- src/services/notices/getNoticesByCategory.js
+++ src/services/notices/getNoticesByCategory.js
@@ -1,12 +1,19 @@
-const { Notices } = require('../../db/noticesModel')
-const { ValidationError } = require('../../helpers/errors')
+const { Notices } = require("../../db/noticesModel");
+const {
+  ValidationError,
+  WrongParametersError,
+} = require("../../helpers/errors");
 
 const getNoticesByCategoryService = async (category) => {
-  if (!['sell', 'lost-found', 'in-good-hands'].includes(category)) {
-    throw new ValidationError(404, 'Not found')
+  if (["sell", "lost-found", "in-good-hands"].includes(category)) {
+    const data = await Notices.find({ categoryName: category });
+    if (!data.length) throw new WrongParametersError("Not found");
+    return data;
   }
-  const data = await Notices.find({ categoryName: category })
-  return data
-}
 
-module.exports = { getNoticesByCategoryService }
+  const data = await Notices.findById(category);
+  if (!data) throw new WrongParametersError("Not found");
+  return data;
+};
+
+module.exports = { getNoticesByCategoryService };
