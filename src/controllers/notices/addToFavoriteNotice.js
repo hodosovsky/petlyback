400d6e5f28a58882ca8ac353514097a7c8665513
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-03-04 19:40:46 +0200

    remove console log

--- src/controllers/notices/addToFavoriteNotice.js
+++ src/controllers/notices/addToFavoriteNotice.js
@@ -4,8 +4,6 @@ const {
 
 const addToFavoriteNotice = async (req, res) => {
   const { _id: userId } = req.user;
-  console.log("_id:", userId);
-
   const { noticeId } = req.params;
   const data = await addToFavoriteNoticeService(noticeId, userId);
   res.json(data);
