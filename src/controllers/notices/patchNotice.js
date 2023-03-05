abdcb9d73aebea61af5e101bbceb07ad24114cc0
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-03-05 11:58:51 +0200

    change return to patch notice

--- src/controllers/notices/patchNotice.js
+++ src/controllers/notices/patchNotice.js
@@ -7,7 +7,7 @@ const patchNoticeController = async (req, res) => {
   const { _id: userId } = req.user._id;
 
   const data = await patchNoticeService(noticesId, userId, req.body);
-  res.json(data);
+  res.json({ message: data });
 };
 
 module.exports = { patchNoticeController };
